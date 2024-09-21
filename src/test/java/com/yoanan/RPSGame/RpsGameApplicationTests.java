954d9afa31fda1bb4ee8e5068cda20358e170b21
Yoana Naydenova <y.naydenova@gmail.com>
2024-09-21 19:09:18 +0300

    Add: initial project files

---
+++ src/test/java/com/yoanan/RPSGame/RpsGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoanan.RPSGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpsGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
