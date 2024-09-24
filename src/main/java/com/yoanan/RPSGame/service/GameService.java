739f4bd6f3a0f29323401480e846ec4ae7bb4c9c
Yoana Naydenova <y.naydenova@gmail.com>
2024-09-24 11:33:45 +0300

    Add playGame nad stopGame methods to GameService

--- src/main/java/com/yoanan/RPSGame/service/GameService.java
+++ src/main/java/com/yoanan/RPSGame/service/GameService.java
@@ -1,7 +1,13 @@
 package com.yoanan.RPSGame.service;
 
 import com.yoanan.RPSGame.dto.GameDto;
+import com.yoanan.RPSGame.dto.MoveDto;
+import com.yoanan.RPSGame.dto.ResultGameDto;
 
 public interface GameService {
     GameDto createGame(Integer attempts);
+
+    ResultGameDto playGame(MoveDto moveDto);
+
+    ResultGameDto stopGame(Long id);
 }
