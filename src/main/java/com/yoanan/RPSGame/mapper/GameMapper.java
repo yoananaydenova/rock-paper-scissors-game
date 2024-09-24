21dc46792eb8060ec4ed0e3d0b7d8c07dfe67f96
Yoana Naydenova <y.naydenova@gmail.com>
2024-09-24 16:24:24 +0300

    Add nullValueMappingStrategy to GameMapper

--- src/main/java/com/yoanan/RPSGame/mapper/GameMapper.java
+++ src/main/java/com/yoanan/RPSGame/mapper/GameMapper.java
@@ -3,8 +3,9 @@ package com.yoanan.RPSGame.mapper;
 import com.yoanan.RPSGame.dto.GameDto;
 import com.yoanan.RPSGame.model.Game;
 import org.mapstruct.Mapper;
+import org.mapstruct.NullValueMappingStrategy;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", nullValueMappingStrategy =  NullValueMappingStrategy.RETURN_DEFAULT)
 public interface GameMapper {
     GameDto toItemDto(Game game);
 }
